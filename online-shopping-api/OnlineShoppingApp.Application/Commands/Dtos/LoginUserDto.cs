12aabc114d410710ec5c83e920546631687614ba
Michel Reynaldo Brito <michelrb2015@gmail.com>
2024-02-13 17:38:06 +0400

    Remove Application Layer

--- online-shopping-api/OnlineShoppingApp.Application/Commands/Dtos/LoginUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineShoppingApp.Application.Commands.Dtos
-{
-    public class LoginUserDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
